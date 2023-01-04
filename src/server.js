7a4d65843ba665075d79371cf9869d6e72e3bc2b
MattVwaves <jamesmattholt@gmail.com>
2023-01-04 15:26:05 +0000

    basic req met

--- src/server.js
+++ src/server.js
@@ -1,13 +1,40 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const contacts = require("../data/contacts.js");
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-// write your app code here
+let idCount = 2;
 
+app.get("/contacts", (req, res) => {
+  res.json(contacts);
+});
 
-module.exports = app
+app.post("/contacts", (req, res) => {
+  idCount += 1;
+  const contact = { id: idCount, ...req.body };
+  contact.id = idCount;
+  contacts.push(contact);
+  res.json(contacts);
+});
+
+app.delete("/contacts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = contacts.find((contact) => contact.id === id);
+  const index = contacts.indexOf(task);
+  contacts.splice(index, 1);
+  res.json(contacts);
+});
+
+app.patch("/contacts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const contact = contacts.find((item) => item.id === id);
+  Object.assign(contact, req.body);
+  res.json(contact);
+});
+
+module.exports = app;

c9499c3765e83e73beee00d8749a7b7bf1f7019a
Dave <Daverevell491@gmail.com>
2023-01-04 17:53:40 +0000

    Functionality for filtering meetings by contactId added

--- src/server.js
+++ src/server.js
@@ -19,65 +19,74 @@ let contacts = require("../data/contacts");
 let meetings = require("../data/meetings");
 // add your routes here
 app.get("/contacts", (req, res) => {
-  res.json(contacts);
+	res.json(contacts);
 });
 
 app.post("/contacts", (req, res) => {
-  const contactData = { ...req.body, id: contacts.length + 1 };
-  contacts.push(contactData);
-  res.status(201).json({ contact: contactData });
+	const contactData = { ...req.body, id: contacts.length + 1 };
+	contacts.push(contactData);
+	res.status(201).json({ contact: contactData });
 });
 
 app.get("/contacts/:id", (req, res) => {
-  const contact = contacts.find((item) => item.id === Number(req.params.id));
-  res.json(contact);
+	const contact = contacts.find((item) => item.id === Number(req.params.id));
+	res.json(contact);
 });
 
 app.delete("/contacts/:id", (req, res) => {
-  const contact = contacts.find((item) => item.id === Number(req.params.id));
-  const index = contacts.indexOf(contact);
-  contacts.splice(index, 1);
-  res.json();
+	const contact = contacts.find((item) => item.id === Number(req.params.id));
+	const index = contacts.indexOf(contact);
+	contacts.splice(index, 1);
+	res.json();
 });
 
 app.put("/contacts/:id", (req, res) => {
-  const contact = contacts.find((item) => item.id === Number(req.params.id));
+	const contact = contacts.find((item) => item.id === Number(req.params.id));
 
-  contact.firstName = req.body.firstName;
-  contact.lastName = req.body.lastName;
-  contact.street = req.body.street;
-  contact.city = req.body.city;
-  contact.type = req.body.type;
-  contact.email = req.body.email;
-  contact.linkedin = req.body.linkedin;
-  contact.twitter = req.body.twitter;
+	contact.firstName = req.body.firstName;
+	contact.lastName = req.body.lastName;
+	contact.street = req.body.street;
+	contact.city = req.body.city;
+	contact.type = req.body.type;
+	contact.email = req.body.email;
+	contact.linkedin = req.body.linkedin;
+	contact.twitter = req.body.twitter;
 
-  console.log(contact);
+	console.log(contact);
 
-  res.status(201).json({ contact: contact });
+	res.status(201).json({ contact: contact });
 });
 
 // Extension
 
 app.get("/meetings", (req, res) => {
-  res.json(meetings);
+	res.json(meetings);
 });
 
 app.get("/meetings/:id", (req, res) => {
-  const meeting = meetings.find((item) => item.id === Number(req.params.id));
-  res.json(meeting);
+	const meeting = meetings.find((item) => item.id === Number(req.params.id));
+	res.json(meeting);
 });
 
 app.delete("/meetings/:id", (req, res) => {
-  const meeting = meetings.find((item) => item.id === Number(req.params.id));
-  const index = meetings.indexOf(meeting);
-  meetings.splice(index, 1);
-  res.json();
+	const meeting = meetings.find((item) => item.id === Number(req.params.id));
+	const index = meetings.indexOf(meeting);
+	meetings.splice(index, 1);
+	res.json();
 });
 
 app.put("/meetings/:id", (req, res) => {
-  const meeting = meetings.find((item) => item.id === Number(req.params.id));
-  meeting.name = req.body.name;
-  res.status(201).json(meeting);
+	const meeting = meetings.find((item) => item.id === Number(req.params.id));
+	meeting.name = req.body.name;
+	res.status(201).json(meeting);
 });
+
+app.get("/contacts/:id/meetings", (req, res) => {
+	const filteredMeetings = meetings.filter(
+		(meeting) => meeting.contactId === req.params.id
+	);
+
+	res.json({ meetings: filteredMeetings });
+});
+
 module.exports = app;

957722ad264896c1818fe78625cb56672be9b108
unknown <katdemey@outlook.com>
2023-03-14 16:11:09 +0000

    all tests passing

--- src/server.js
+++ src/server.js
@@ -8,10 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // write your app code here
-let contactId = 2;
+let id = 2;
 
 const contacts = require("../data/contacts");
-
 
 app.get("/contacts", (req, res) => {
   res.json({ contacts });
@@ -23,35 +22,31 @@ app.get("/contacts/:id", (req, res) => {
   res.json({ contact });
 });
 
-// app.post("/contacts", (req, res) => {
-//   contactId++;
-//   createTestFormData.id = contactId;
-//   contacts.push(createTestFormData);
-//   res.status(201).json({ contacts });
-// });
+app.post("/contacts", (req, res) => {
+  id++;
+  const newContact = { ...req.body, id };
+  contacts.push(newContact);
+  res.status(201).json({ contact: newContact });
+});
 
-app.post('/contacts', (req, res) => {
-    contactId++
-    createTestFormData.id = contactId
-    contacts.push(createTestFormData)
-    res.status(201).json({contact: createTestFormData})
-})
+app.delete("/contacts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = contacts.findIndex((person) => person.id === id);
+  const contact = contacts.splice(index, 1)[0];
+  res.json({ contact });
+});
 
-app.delete('/contacts/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const index = contacts.findIndex((person) => person.id === id)
-    const contact = contacts.splice(index, 1)[0]
-    res.json({contact})
-})
+app.put("/contacts/:id", (req, res) => {
+  const contact = contacts.find(
+    (person) => person.id === Number(req.params.id)
+  );
+  console.log("updated", contact);
+  const updatedContact = { ...contact, ...req.body };
+  contacts[contacts.indexOf(contact)] = updatedContact;
 
-app.put('/contacts/:id', (req, res) => {
-    const contact = contacts.find((person) => person.id === Number(req.params.id))
-    console.log("updated" , contact)
-    const updatetedContact = {...contact, ...req.body}  
+  console.log("req.body", req.body);
 
-    console.log("req.body", req.body)
-    
-    res.send({contact: updatetedContact})
-})
+  res.send({ contact: updatedContact });
+});
 
 module.exports = app;

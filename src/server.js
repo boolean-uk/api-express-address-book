075692e705d9bd2f446491450e5276bb847a4363
desokan <thefdmm@gmail.com>
2023-06-27 12:37:28 +0100

    got a single contact by id

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
+
+
 // 1. Retrieve a list of contacts
 app.get("/contacts", (req, res) => {
   return res.send({ contacts });
@@ -20,6 +22,20 @@ app.post("/contacts", (req, res) => {
   contact.id = id;
   contacts.push(contact);
   return res.status(201).send({ contact });
+});
+
+// 3. Get a single contact by ID
+app.get("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+  const contact = contacts.find((contact) => {
+    return (contact.id === Number(id));
+  });
+
+  if (contact) {
+    return res.send({ contact });
+  } else {
+    return res.send("Contact does not exist");
+  }
 });
 
 module.exports = app;

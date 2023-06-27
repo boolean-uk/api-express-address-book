17362557b596c1d0f71f783ae46377ffbddbed81
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-06-27 18:41:10 +0100

    get request done, test passing

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
+const contacts = require("../data/contacts");
+app.get("/contacts", (req, res) => {
+  res.send({ contacts: contacts });
+});
 
-module.exports = app
+app.get("/contacts/:id", (req, res) => {
+  const contact = contacts.find((item) => item.id === Number(req.params.id));
+  res.send({ contact });
+});
+
+module.exports = app;

8309a0f47e9361507c10c03a1bc118e58380d32d
NoroAxper <norikvardanyan.w@gmail.com>
2023-06-27 12:17:00 +0100

    get not yet working

--- src/server.js
+++ src/server.js
@@ -8,6 +8,15 @@ app.use(cors())
 app.use(express.json())
 
 // write your app code here
+const contacts = []
 
+app.get('/contacts', (req, res) => {
+    return res.send(contacts)
+})
+
+app.post('/contacts', (req, res) => {
+    const body = req.body
+    return res.status(200).send(body)
+})
 
 module.exports = app

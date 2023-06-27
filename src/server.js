528cf3431f0c74830823b99a5fd3a1d5d1ebcf55
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-27 14:50:01 +0200

    post request done

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const { firstName } = require("../test/fixtures/contacts/createTestFormData")
 const app = express()
+const contacts = require("../data/contacts")
 
 app.use(morgan("dev"))
 app.use(cors())
@@ -9,5 +11,15 @@ app.use(express.json())
 
 // write your app code here
 
+app.get("/contacts", (req, res) => {
+    return res.send ( {contacts})
+})
+
+app.post("/contacts", (req, res) => {
+    const newContact = req.body
+    newContact.id = contacts[contacts.length -1].id + 1
+    contacts.push(newContact)
+    return res.status(201).send ( { newContact })
+})
 
 module.exports = app

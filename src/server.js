5641942b7a8db772b53485efcbffab55e2270e41
Sara Beazley <sarajbeazley@gmail.com>
2023-06-27 14:04:53 -0700

    get contacts

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const contacts = require("../data/contacts");
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-// write your app code here
+// Get contacts
+app.get('/contacts', (req, res) => {
+  console.log(contacts);
+  return res.send({ contacts })
+})
 
 
-module.exports = app
+
+module.exports = app;

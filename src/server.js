9a4630a77badd890df907e061ddab3edc5b45c62
desokan <thefdmm@gmail.com>
2023-06-27 13:03:09 +0100

    Deleted a single contact by id

--- src/server.js
+++ src/server.js
@@ -8,7 +8,12 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-
+const findContactById = (id) => {
+  const result = contacts.find((contact) => {
+    return contact.id === Number(id);
+  });
+  return result;
+};
 
 // 1. Retrieve a list of contacts
 app.get("/contacts", (req, res) => {
@@ -27,11 +32,26 @@ app.post("/contacts", (req, res) => {
 // 3. Get a single contact by ID
 app.get("/contacts/:id", (req, res) => {
   const { id } = req.params;
-  const contact = contacts.find((contact) => {
-    return (contact.id === Number(id));
+  const contact = findContactById(id);
+
+  if (contact) {
+    return res.send({ contact });
+  } else {
+    return res.send("Contact does not exist");
+  }
+});
+
+// 4. Delete a single contact by ID
+app.delete("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+  const contact = findContactById(id);
+
+  const conIndex = contacts.findIndex((item) => {
+    return item === contact;
   });
 
   if (contact) {
+    const contact = contacts.splice(conIndex, 1);
     return res.send({ contact });
   } else {
     return res.send("Contact does not exist");

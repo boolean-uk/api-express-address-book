65f5582e6aee8f74a97d78872f70945403e5600f
desokan <thefdmm@gmail.com>
2023-06-27 12:07:28 +0100

    retireved a list of contacts

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const contacts = require("../data/contacts");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
+// console.log(contacts);
 
-module.exports = app
+// 1. Retrieve a list of contacts
+
+app.get('/contacts', (req, res) => {
+    return res.send({contacts})
+})
+
+
+module.exports = app;

bc38c38ca1dfd209ad88ec99095561272f25295c
Alex Shaw <alexjshaw@gmail.com>
2023-06-27 14:27:23 +0100

    get by ID and delete by ID working

--- src/server.js
+++ src/server.js
@@ -11,15 +11,41 @@ app.use(express.json())
 
 // write your app code here
 
+// Get all contacts
 app.get("/contacts", (req, res) => {
     return res.send ( {contacts})
 })
 
+// Create new contact
 app.post("/contacts", (req, res) => {
     const newContact = req.body
     newContact.id = contacts[contacts.length -1].id + 1
     contacts.push(newContact)
-    return res.status(201).send ( { newContact })
+    return res.status(201).send ( { contact: newContact })
+})
+
+// Get contact by ID
+app.get('/contacts/:id', (req, res) => {
+  const id = req.params.id
+  const contact = contacts.find((item) => item.id === Number(id))
+  if (contact) {
+    return res.send( { contact } )  
+  } else {
+    return res.send('No Contact Found')
+  }
+})
+
+// Delete a contact by ID
+app.delete('/contacts/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const targetContact = contacts.find((item) => item.id === id)
+  const deletionIndex = contacts.indexOf(targetContact)
+  contacts.splice(deletionIndex, 1)
+  if (targetContact) {
+    return res.send( { contact: targetContact } )
+  } else {
+    return res.send('No Contact Found')
+  }
 })
 
 module.exports = app

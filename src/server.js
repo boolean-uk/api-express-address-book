623832188fa416dba9fdfbc8dc14b49f9c89cb2a
Todd Griffin <todd.griffin61@outlook.com>
2023-06-27 14:20:49 +0100

    finished contacts pt3

--- src/server.js
+++ src/server.js
@@ -14,6 +14,20 @@ app.get('/contacts', (req, res) => {
   return res.send({ contacts })
 })
 
+// Create a new contact
+app.post('/contacts', (req, res) => {
+  const newContact = req.body
+  contacts.push(newContact)
+  return res.status(201).send(newContact)
+})
+
+// Find contact by id
+app.get('/contacts/:id', (req, res) => {
+  const id = req.params.id
+  const contact = contacts.find(contact => contact.id === Number(id))
+
+  return res.status(200).send(contact)
+})
 
 
 module.exports = app;

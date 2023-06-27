587e113ffb2d21089c908a86861e1fed23c6b568
desokan <thefdmm@gmail.com>
2023-06-27 13:49:46 +0100

    completed core

--- src/server.js
+++ src/server.js
@@ -25,14 +25,13 @@ app.post("/contacts", (req, res) => {
   const contact = req.body;
   const id = contacts.push(contact);
   contact.id = id;
-  contacts.push(contact);
   return res.status(201).send({ contact });
 });
 
 // 3. Get a single contact by ID
 app.get("/contacts/:id", (req, res) => {
   const { id } = req.params;
-  const contact = findContactById(id);
+  const contact = findContactById(Number(id));
 
   if (contact) {
     return res.send({ contact });
@@ -44,15 +43,37 @@ app.get("/contacts/:id", (req, res) => {
 // 4. Delete a single contact by ID
 app.delete("/contacts/:id", (req, res) => {
   const { id } = req.params;
-  const contact = findContactById(id);
+  const contact = findContactById(Number(id));
 
   const conIndex = contacts.findIndex((item) => {
     return item === contact;
   });
 
   if (contact) {
-    const contact = contacts.splice(conIndex, 1);
+    const contact = contacts.splice(conIndex, 1)[0];
+    console.log(contact);
     return res.send({ contact });
+  } else {
+    return res.send("Contact does not exist");
+  }
+});
+
+// Update a contact by ID
+app.put("/contacts/:id", (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const contact = findContactById(Number(id));
+
+  const updatedContact = { ...body, id: Number(id) };
+  console.log(updatedContact);
+
+  const conIndex = contacts.findIndex((item) => {
+    return item === contact;
+  });
+
+  if (contact) {
+    contacts.splice(conIndex, 1, updatedContact);
+    return res.send({ contact: contacts[conIndex] });
   } else {
     return res.send("Contact does not exist");
   }

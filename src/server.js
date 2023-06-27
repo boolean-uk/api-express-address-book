38bf75c7b95ffecfe319d4031722b98b92206415
Sara Beazley <sarajbeazley@gmail.com>
2023-06-27 14:58:05 -0700

    delete single contact by id

--- src/server.js
+++ src/server.js
@@ -9,25 +9,34 @@ app.use(cors());
 app.use(express.json());
 
 // Get contacts
-app.get('/contacts', (req, res) => {
+app.get("/contacts", (req, res) => {
   console.log(contacts);
-  return res.send({ contacts })
-})
+  return res.send({ contacts });
+});
 
 // Create a new contact
-app.post('/contacts', (req, res) => {
-  const newContact = req.body
-  contacts.push(newContact)
-  return res.status(201).send(newContact)
-})
+app.post("/contacts", (req, res) => {
+  const newContact = req.body;
+  contacts.push(newContact);
+  return res.status(201).send(newContact);
+});
 
 // Find contact by id
-app.get('/contacts/:id', (req, res) => {
-  const id = req.params.id
-  const contact = contacts.find(contact => contact.id === Number(id))
+app.get("/contacts/:id", (req, res) => {
+  const id = req.params.id;
+  const contact = contacts.find((contact) => contact.id === Number(id));
 
-  return res.status(200).send(contact)
-})
+  return res.status(200).send(contact);
+});
 
+// Delete a single contact by id
+app.delete("/contacts/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedContact =  contacts.find((contact) => contact.id === Number(id))
+  const contactIndex = contacts.indexOf(deletedContact);
+  contacts.splice(contactIndex, 1);
+//   return res.send({ contacts: contactIndex });
+  return res.status(200).send(deletedContact);
+});
 
 module.exports = app;

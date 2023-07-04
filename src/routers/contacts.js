c53202a2819f5b630c7785de7fe3d5b7e04a9e17
Isabelle <isabel.tartarelli@live.it>
2023-07-04 21:00:57 +0100

    Created Contact

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,9 +2,22 @@ const express = require('express')
 const router = express.Router()
 const contacts = require('../../data/contacts')
 
+let newId = contacts.length
 // Get all list
 router.get('/', (req, res) => {
 	return res.send({ contacts })
 })
+// Create contact
+router.post('/', (req, res) => {
+	newId++
+	const newContact = {
+		...req.body,
+		id: newId,
+	}
 
+	contacts.push(newContact)
+	return res.status(201).send({
+		newContact,
+	})
+})
 module.exports = router

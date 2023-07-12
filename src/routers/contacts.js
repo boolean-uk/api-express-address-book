d7dafae845d2e225cdb28a54b7d614addcdd8084
Isabelle <isabel.tartarelli@live.it>
2023-07-12 11:18:12 +0100

    All test passing

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -16,9 +16,7 @@ router.post('/', (req, res) => {
 		id: newId,
 	}
 	contacts.push(newContact)
-	return res.status(201).send({
-		newContact,
-	})
+	return res.status(201).send({ contact: newContact })
 })
 
 // Contact by ID
@@ -33,16 +31,13 @@ router.delete('/:id', (req, res) => {
 	const id = Number(req.params.id)
 	const contactIndex = contacts.findIndex((contact) => contact.id === id)
 	const deletedContact = contacts.splice(contactIndex, 1)[0]
-	return res.send({ remainingContacts: contacts })
+	return res.send({ contact: deletedContact })
 })
 
 // Update by ID
 router.put('/:id', (req, res) => {
-	console.log('Put request')
 	const id = Number(req.params.id)
 	const body = req.body
-	console.log('id', id)
-	console.log('body', body)
 	let contactToUpdate = contacts.find((contact) => contact.id === id)
 	Object.assign(contactToUpdate, body)
 	return res.status(200).send({ contact: contactToUpdate })

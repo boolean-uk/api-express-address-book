4258e8f137794a37fd24bb94b45927c902639a40
irlydo <fokum8@gmail.com>
2024-01-03 19:15:55 +0000

    All set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./server.js')
-const port = 3030
+const port = 3031
 
 app.listen(port, () => {
  console.log(`Server is running on http://localhost:${port}/`)
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-// write your app code here
+let contacts = []
 
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "Hello World!!" });
+});
 
-module.exports = app
+app.get("/contacts", (req, res) => {
+  res.status(200).json([contacts]);
+});
+
+module.exports = app;

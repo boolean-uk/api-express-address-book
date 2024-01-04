4c1e357e7f9ef1652b694b450122f413106bdfc9
Satoki <satokiito@gmail.com>
2024-01-04 12:15:02 +0000

    get request added to retrieve list of all contacts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
+const data = require("../data/contacts.js")
 
 app.use(morgan("dev"))
 app.use(cors())
@@ -9,5 +10,13 @@ app.use(express.json())
 
 // write your app code here
 
+app.get("/contacts", (req, res) => {
+
+    const allContacts = {
+        "contacts": data
+    }
+
+    return res.status(200).json(allContacts)
+})
 
 module.exports = app

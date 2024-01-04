1dbd88f22cd312ad03fd432f97230816dc58efd4
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-04 22:48:36 +0000

    Add /contacts endpoint

--- src/server.js
+++ src/server.js
@@ -3,10 +3,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+// import data
+const contacts = require('../data/contacts')
+
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
 // write your app code here
+
+// Retrieve a list of contacts
+app.get('/contacts', (req, res, next) => {
+  if (!Array.isArray(contacts) || contacts.length === 0) {
+    return res.status(400).json({
+      message: 'Contacts must be non-empty array'
+    })
+  }
+
+  res.status(200).json({ contacts })
+})
 
 module.exports = app

c45d4ad6769a11c40f716b580361086442ab8da0
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-04 13:55:23 +0100

    fix error in getContactById

--- data/contacts.js
+++ data/contacts.js
@@ -26,7 +26,7 @@ const contacts = [
 let contactId = 0
 
 const getNewContactId = () => ++contactId
-const getContactById = (id) => contacts.find((contact) => contacts.id === id)
+const getContactById = (id) => contacts.find((contact) => contact.id === id)
 const deleteContactById = (id) => contacts.filter((contact) => contact.id !== id)
 
 const addContact = (

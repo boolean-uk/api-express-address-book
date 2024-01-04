f3e0e4f480b5ca464510c3fa2be17a9187e97637
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-04 12:09:12 +0000

    feat: add retrieve all contacts

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
+const contactsData = require("../data/contacts")
 
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
+
+app.get('/contacts', (req, res) => {
+    return res.json({"contacts": contactsData})
+})
 
 module.exports = app

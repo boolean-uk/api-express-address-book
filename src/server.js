159ebc1f3a51940911d0ac815b760d097461e65c
Faiza <faraz_ul_islam@yahoo.com>
2024-01-04 11:42:26 +0000

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+//import contacts  from "../data/contacts"
+const contacts = require('../data/contacts')
+
+
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
@@ -8,6 +12,11 @@ app.use(cors())
 app.use(express.json())
 
 // write your app code here
+
+
+app.get('/contacts', (req , res) => {
+    res.json('hello')
+})
 
 
 module.exports = app

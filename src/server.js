7b1b592e3dce96f094cac2f44995aa61a9f2c3b5
Elizabeth <lizbeepmc@gmail.com>
2024-01-04 17:24:15 +0300

    get contact by id

--- src/server.js
+++ src/server.js
@@ -34,5 +34,25 @@ app.post('/contacts', (req, res) => {
     // 201 = successfully created HTTP status code
     res.status(201).json({ contacts: newContact })
   })
+  app.get('/contacts/:id', (req, res) => {
+    // `req.params.id` is a string
+    const contactId = Number(req.params.id)
+  
+    // Query the database to find the relevant post
+    // Along the way, you should do some checks to make sure the user is authorised to do this:
+    // Check if the post with ID `postId` exists
+    // If the posts exists, then send the post back to the user who requested it.
+    // If the post does not exist, then send an error message
+  
+    const foundContact = contacts.find((contact) => contact.id === contactId)
+  
+    if (!foundContact) {
+      return res.status(404).json({
+        message: `No contact with id ${contactId} exists!`
+      })
+    }
+  
+    return res.json({ contact: foundContact });
+  })
 
 module.exports = app

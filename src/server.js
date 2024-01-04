daa49cbfa2ab1caefba9a1c3102c553c6935c1ed
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-04 13:02:37 +0000

    feat: add 'update a contact'

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-const findContactBy = (id) => (contacts.find((contact) => contact.id === id));
+const findContactBy = (id) => contacts.find((contact) => contact.id === id);
 const getIdFromParams = (params) => {
-	const { id } = params;
+  const { id } = params;
   const idNum = parseInt(id);
-	return idNum
-}
+  return idNum;
+};
 
 app.get("/contacts", (req, res) => {
   return res.json({ contacts: contacts });
@@ -28,20 +28,33 @@ app.post("/contacts", (req, res) => {
   return res.status(201).json({ contact: newContact });
 });
 
-app.get("/contacts/:id", (req, res) => { 
-	const id = getIdFromParams(req.params)
+app.get("/contacts/:id", (req, res) => {
+  const id = getIdFromParams(req.params);
   const foundContact = findContactBy(id);
-  return res.json({ contact: foundContact});
+  return res.json({ contact: foundContact });
 });
 
 app.delete("/contacts/:id", (req, res) => {
-	const id = getIdFromParams(req.params)
-	const foundContact = findContactBy(id)
-	const index = contacts.indexOf(foundContact)
+  const id = getIdFromParams(req.params);
+  const foundContact = findContactBy(id);
+  const index = contacts.indexOf(foundContact);
 
-	contacts.splice(index, 1)
+  contacts.splice(index, 1);
 
-	return res.json({"contact": foundContact})
-})
+  return res.json({ contact: foundContact });
+});
+
+app.put("/contacts/:id", (req, res) => {
+	const id = getIdFromParams(req.params);
+  const foundContact = findContactBy(id);
+  const index = contacts.indexOf(foundContact);
+	const updatedContact = req.body;
+
+
+	updatedContact.id = id;
+	contacts.splice(index, 1, updatedContact)
+
+	return res.json({"contact": updatedContact})
+});
 
 module.exports = app;

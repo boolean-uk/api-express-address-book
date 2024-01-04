56c80760d6a51cef73173072a97fd8730fc25a05
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-04 16:43:19 +0000

    style: minor prettierand whitespace improvements

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const contacts = require("../data/contacts");
-const meetings = require("../data/meetings")
+const meetings = require("../data/meetings");
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -12,21 +12,20 @@ app.use(express.json());
 const findContactBy = (id) => contacts.find((contact) => contact.id === id);
 const findMeetingBy = (id) => meetings.find((meeting) => meeting.id === id);
 const deleteMeetingsBy = (contactId) => {
-	meetings.forEach((meeting) => {
-		if (meeting.contactId === contactId) {
-			const index = meetings.indexOf(meeting)
-			meetings.splice(index, 1)
-		}
-	})
-}
-
-// CORE
+  meetings.forEach((meeting) => {
+    if (meeting.contactId === contactId) {
+      const index = meetings.indexOf(meeting);
+      meetings.splice(index, 1);
+    }
+  });
+};
 const getIdFromParams = (params) => {
   const { id } = params;
   const idNum = parseInt(id);
   return idNum;
 };
 
+// CORE
 app.get("/contacts", (req, res) => {
   return res.json({ contacts: contacts });
 });
@@ -52,75 +51,74 @@ app.delete("/contacts/:id", (req, res) => {
   const index = contacts.indexOf(foundContact);
 
   contacts.splice(index, 1);
-
-	// EXTENSION CRITERIA (deleting a contact also delete their meetings)
-	deleteMeetingsBy(id)
+  // extension: deleting a contact also delete their meetings:
+  deleteMeetingsBy(id);
 
   return res.json({ contact: foundContact });
 });
 
 app.put("/contacts/:id", (req, res) => {
-	const id = getIdFromParams(req.params);
+  const id = getIdFromParams(req.params);
   const foundContact = findContactBy(id);
   const index = contacts.indexOf(foundContact);
-	const updatedContact = req.body;
+  const updatedContact = req.body;
 
-	updatedContact.id = id;
-	contacts.splice(index, 1, updatedContact)
+  updatedContact.id = id;
+  contacts.splice(index, 1, updatedContact);
 
-	return res.json({"contact": updatedContact})
+  return res.json({ contact: updatedContact });
 });
 
 // EXTENSION
+app.get("/meetings", (req, res) => res.json({ meetings: meetings }));
 
-app.get('/meetings', (req, res) => (res.json({"meetings": meetings})))
+app.get("/meetings/:id", (req, res) => {
+  const id = getIdFromParams(req.params);
+  const foundMeeting = findMeetingBy(id);
+  return res.json({ meeting: foundMeeting });
+});
 
-app.get('/meetings/:id', (req, res) => {
-	const id = getIdFromParams(req.params)
-	const foundMeeting = findMeetingBy(id)
-	return res.json({"meeting": foundMeeting})
-})
-
-app.delete('/meetings/:id', (req, res) => {
-	const id = getIdFromParams(req.params)
-	const foundMeeting = findMeetingBy(id)
+app.delete("/meetings/:id", (req, res) => {
+  const id = getIdFromParams(req.params);
+  const foundMeeting = findMeetingBy(id);
   const index = meetings.indexOf(foundMeeting);
 
-	meetings.splice(index, 1)
-	return res.json({"meeting": foundMeeting})
-})
+  meetings.splice(index, 1);
+  return res.json({ meeting: foundMeeting });
+});
 
-app.put('/meetings/:id', (req, res) => {
-	const id = getIdFromParams(req.params)
-	const updatedMeeting = req.body
-	const foundMeeting = findMeetingBy(id)
-  const index = meetings.indexOf(foundMeeting)
+app.put("/meetings/:id", (req, res) => {
+  const id = getIdFromParams(req.params);
+  const updatedMeeting = req.body;
+  const foundMeeting = findMeetingBy(id);
+  const index = meetings.indexOf(foundMeeting);
 
-	updatedMeeting.id = id
-	updatedMeeting.contactId = foundMeeting.contactId
-	meetings.splice(index, 1, updatedMeeting)
+  updatedMeeting.id = id;
+  updatedMeeting.contactId = foundMeeting.contactId;
+  meetings.splice(index, 1, updatedMeeting);
 
-	return res.json({"meeting": updatedMeeting})
-})
+  return res.json({ meeting: updatedMeeting });
+});
 
-app.get('/contacts/:id/meetings', (req, res) => {
-	const contactId = getIdFromParams(req.params)
-	const meetingsForContact = meetings.filter((meeting) => meeting.contactId === contactId)
+app.get("/contacts/:id/meetings", (req, res) => {
+  const contactId = getIdFromParams(req.params);
+  const meetingsForContact = meetings.filter(
+    (meeting) => meeting.contactId === contactId
+  );
 
-	return res.json({"meetings": meetingsForContact})
-})
+  return res.json({ meetings: meetingsForContact });
+});
 
-app.post('/contacts/:id/meetings', (req, res) => {
-	const id = meetings.length + 1
-	const contactId = getIdFromParams(req.params)
-	const newMeeting = req.body
+app.post("/contacts/:id/meetings", (req, res) => {
+  const id = meetings.length + 1;
+  const contactId = getIdFromParams(req.params);
+  const newMeeting = req.body;
 
-	newMeeting.id = id
-	newMeeting.contactId = contactId
+  newMeeting.id = id;
+  newMeeting.contactId = contactId;
+  meetings.push(newMeeting);
 
-	meetings.push(newMeeting)
-
-	return res.status(201).json({"meeting": newMeeting})
-})
+  return res.status(201).json({ meeting: newMeeting });
+});
 
 module.exports = app;

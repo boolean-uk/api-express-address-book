84fc8ca70c53417c406df7ead28344620c19c719
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-04 11:59:52 +0000

    feat: get /contacts added

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+
+const contacts = require("../data/contacts.js")
+
 const app = express()
 
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-// write your app code here
-app.get("/", (req , res) => {
-    res.json()
-})
+const STATE = {
+  contacts,
+}
 
+app.get("/contacts", (req, res) => {
+  res.json({contacts: STATE.contacts})
+})
 
 module.exports = app

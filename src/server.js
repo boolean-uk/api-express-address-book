8424c67a356540afac947cf22e01ff3b56d0851d
Lukas Dembicki <lad_777@hotmail.com>
2024-01-04 11:43:33 +0000

    ADD: GET- Welcome message on '/' and retrieves all contacts on /contacts

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+
+const contacts = require('./data.js')
 const app = express()
 
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-// write your app code here
+
+app.get('/', (req, res) => {
+ return res.status(200).json('Welcome to contacts')
+})
+
+app.get('/contacts', (req, res) => {
+    return res.status(200).json(contacts)
+})
 
 
 module.exports = app

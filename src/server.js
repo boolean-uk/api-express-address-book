e70fa45d5413a903e2113587707c603eddd4f3d1
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-04 23:06:02 +0000

    Add PUT /contacts endpoint create contact

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const app = express()
 // import data
 const contacts = require('../data/contacts')
 
+// Global variables
+let idCounter = contacts.length + 1
+
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
@@ -21,6 +24,23 @@ app.get('/contacts', (req, res, next) => {
   }
 
   res.status(200).json({ contacts })
+})
+
+// Create a contact
+app.post('/contacts', (req, res, next) => {
+  const data = req.body
+
+  for (let key in data) {
+    if (typeof data[key] !== 'string') {
+      return res
+        .status(400)
+        .json({ message: 'Bad request, all the items must be string' })
+    }
+  }
+
+  contacts.push({ id: idCounter++, ...data })
+
+  res.status(201).json({ ...data })
 })
 
 module.exports = app

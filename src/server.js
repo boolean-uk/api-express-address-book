9cecb08902dbe98dccf6062565cbb768166e7e72
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-01-05 00:09:17 +0100

    code added for the contacts end points

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
 
+const contactsData = require("../data/contacts.js");
+const meetingsData = require("../data/meetings.js");
 
-module.exports = app
+const appState = {
+  contacts: contactsData,
+  nextContactId: 3,
+  meetings: meetingsData,
+  nextMeetingId: 4,
+};
+
+app.get("/contacts", (req, res) => {
+  res.json({ contacts: appState.contacts });
+});
+
+module.exports = app;

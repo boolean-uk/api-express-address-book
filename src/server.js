8e2a877fbe56d594e50c014d36486a513155643a
Elizabeth <lizbeepmc@gmail.com>
2024-01-04 15:46:28 +0300

    get test passed

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
+const contacts = require("../data/contacts.js")
 
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
 // write your app code here
-
+app.get('/contacts', (req, res) => {
+    res.status(200).json(contacts)
+})
 
 module.exports = app

a8010fa44ff4fa607f19650adb1507810c5a6a01
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-04 22:35:08 +0000

    Update packaje.json

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
 // write your app code here
-
 
 module.exports = app

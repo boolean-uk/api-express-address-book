72fad59a2cc7a6914d2bbcd9b77c7718780fa70b
ilham saleh <ilham2saleh@gmail.com>
2024-01-04 13:45:25 +0000

    Return a single contact by ID - test passed

--- src/server.js
+++ src/server.js
@@ -3,23 +3,34 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
-const createTestFormData = require("../test/fixtures/contacts/createTestFormData.js");
-const createTestupdateTestFormDataFormData = require("../test/fixtures/contacts/updateTestFormData.js");
+const contacts = require("../data/contacts.js");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
 // write your app code here
-let currentID = 1;
+const findContact = (req, res) => {
+  const contactId = Number(req.params.contactId);
 
-const contacts = createTestFormData.map((contact, index) => ({
-  id: currentID + index,
-  ...contact,
-}));
+  const foundContact = contacts.find((contact) => contact.id === contactId);
+
+  if (!foundContact) {
+    res.status(404).json({ message: `no such contact with id: ${contactId}` });
+  }
+
+  return foundContact;
+};
+let currentID = 2;
 
 app.get("/contacts", (req, res) => {
   return res.status(200).json({ contacts });
+});
+
+app.get("/contacts/:contactId", (req, res) => {
+  const contact = findContact(req, res);
+
+  res.status(200).json({ contact });
 });
 
 module.exports = app;
--- test/fixtures/contacts/createTestFormData.js
+++ test/fixtures/contacts/createTestFormData.js
@@ -1,25 +1,12 @@
-const createTestFormData = [
-  {
-    firstName: "John",
-    lastName: "Carmack",
-    street: "testStreet",
-    city: "testCity",
-    type: "testType",
-    email: "test@test.com",
-    linkedin: "http:test.com",
-    twitter: "http:test.com",
-  },
-
-  {
-    firstName: "Grace",
-    lastName: "Hopper",
-    street: "testStreet",
-    city: "testCity",
-    type: "testType",
-    email: "test@test.com",
-    linkedin: "http:test.com",
-    twitter: "http:test.com",
-  },
-];
+const createTestFormData = {
+  firstName: "John",
+  lastName: "Carmack",
+  street: "testStreet",
+  city: "testCity",
+  type: "testType",
+  email: "test@test.com",
+  linkedin: "http:test.com",
+  twitter: "http:test.com",
+};
 
 module.exports = createTestFormData;

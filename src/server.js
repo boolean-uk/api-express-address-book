04c4c5cfc7c488e70d438db91b12e459769c09de
Loza_TM <lozatmamo@gmail.com>
2024-01-07 22:16:32 +0100

    initiating express app and logging and handling cors

--- src/server.js
+++ src/server.js
@@ -9,5 +9,15 @@ app.use(express.json())
 
 // write your app code here
 
+const contacts = require("../data/contacts.js");
+const meetings = require("../data/meetings.js");
+
+// initializing express app
+const app = express();
+
+// Middleware setup for logging and handling CORS
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 module.exports = app

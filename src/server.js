2a05035668a135e3a0aa57b5decfce1df9d8eae4
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-07 20:40:00 +0100

    added the get req for contacts

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const data = require('../data/contacts.js')
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
 // write your app code here
+app.get("/contacts", (req, res) => {
+  const allContacts = {
+    contacts: data,
+  };
 
+  return res.status(200).json(allContacts);
+});
 
-module.exports = app
+module.exports = app;

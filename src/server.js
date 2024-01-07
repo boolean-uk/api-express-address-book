23a4bcb2e2a909fd6ca9cbb148d763ec356cce2b
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-07 20:55:02 +0100

    added post req to create a new contact

--- src/server.js
+++ src/server.js
@@ -2,19 +2,46 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
-const data = require('../data/contacts.js')
+const data = require("../data/contacts.js");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
 // write your app code here
+
+let currentId = 2;
 app.get("/contacts", (req, res) => {
   const allContacts = {
     contacts: data,
   };
 
   return res.status(200).json(allContacts);
+});
+
+app.post("/contacts", (req, res) => {
+  const { firstName, lastName, street, city, type, email, linkedin, twitter } =
+    req.body;
+
+  const newContact = {
+    id: ++currentId,
+    firstName,
+    lastName,
+    street,
+    city,
+    type,
+    email,
+    linkedin,
+    twitter,
+  };
+
+  data.push(newContact)
+
+  const returnedContact = {
+    "contact": newContact
+  }
+
+  return res.status(201).json(returnedContact)
 });
 
 module.exports = app;

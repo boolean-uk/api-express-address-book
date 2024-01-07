f9aefcffd4449a7a4413f6efc0f3324d044567a9
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-07 11:39:02 +0100

    add helper meeting functions

--- data/meetings.js
+++ data/meetings.js
@@ -16,4 +16,42 @@ const meetings = [
   }
 ]
 
-module.exports = meetings
+let meetingId = 0
+
+const initMeetingId = () => {
+  meetings.forEach((meeting) => {
+    if (meeting.id > meetingId) {
+      meetingId = meeting.id
+    }
+  })
+}
+
+initMeetingId()
+
+const getNewMeetingId = () => ++meetingId
+const getMeetingById = (id) => meetings.find((meeting) => meeting.id === id)
+const deleteMeetingById = (id) => {
+  const index = meetings.findIndex((meeting) => meeting.id === id)
+  const deletedMeeting = meetings[index]
+  meetings.splice(index, 1)
+  return deletedMeeting
+}
+
+class Meeting {
+  constructor(name, contactId) {
+    this.id = getNewMeetingId()
+    this.name = name
+    this.contactId
+  } 
+}
+
+const addMeeting = (name, contactId) => {
+  meetings.push(new Meeting(name, contactId))
+}
+
+module.exports = {
+  meetings,
+  addMeeting,
+  getMeetingById,
+  deleteMeetingById
+}

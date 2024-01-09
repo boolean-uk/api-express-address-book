ae684ecbe79c34a80c5e49e39cabd9184ad39599
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-09 00:43:05 +0000

    core finished

--- src/server.js
+++ src/server.js
@@ -1,13 +1,61 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const contactsData = require('../data/contacts');
 
-app.use(morgan("dev"))
-app.use(cors())
-app.use(express.json())
+const app = express();
 
 
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/contacts', (req, res) => {
+  res.status(200).json({ contacts: contactsData });
+});
 
 
-module.exports = app
+app.get('/contacts/:id', (req, res) => {
+  const { id } = req.params;
+  const contact = contactsData.find((contact) => contact.id === parseInt(id));
+  if (!contact) {
+    res.status(404).json({ message: 'Contact not found' });
+  } else {
+    res.status(200).json({ contact });
+  }
+});
+
+app.post('/contacts', (req, res) => {
+  const newContact = req.body;
+  newContact.id = contactsData.length + 1;
+  contactsData.push(newContact);
+  res.status(201).json({ contact: newContact });
+});
+
+
+app.put('/contacts/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedContact = req.body;
+  const index = contactsData.findIndex((contact) => contact.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).json({ message: 'Contact not found' });
+  } else {
+    contactsData[index] = { ...contactsData[index], ...updatedContact };
+    res.status(200).json({ contact: contactsData[index] });
+  }
+});
+
+
+app.delete('/contacts/:id', (req, res) => {
+  const { id } = req.params;
+  const index = contactsData.findIndex((contact) => contact.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).json({ message: 'Contact not found' });
+  } else {
+    const deletedContact = contactsData.splice(index, 1);
+    res.status(200).json({ contact: deletedContact[0] });
+  }
+});
+
+
+module.exports = app;
